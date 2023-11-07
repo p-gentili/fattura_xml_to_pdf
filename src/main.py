095f0d267ab3876719a65a2e0cd52437cc5387b1
Paolo Gentili <paolo.gentili@canonical.com>
2023-11-07 11:49:03 +0200

    Change: providing the xsl in the package

--- src/main.py
+++ src/main.py
@@ -1,24 +1,15 @@
 import argparse
+import os
 from lxml import etree
 from io import BytesIO
 from xhtml2pdf import pisa
 
 
-def convert(xml_file: str, xsl_file: str):
-    try:
-        with open(xml_file, "rb") as xml_input:
-            xml_tree = etree.parse(xml_input)
+def convert(xml_tree: bytes, xsl_root: bytes):
+    """Apply the style and convert to PDF."""
 
-        with open(xsl_file, "rb") as xsl_input:
-            xsl_root = etree.XML(xsl_input.read())
-    except FileNotFoundError as exc:
-        raise SystemExit("Cannot open input files.") from exc
-
-    # Apply the XSL transformation to the XML data
     transform = etree.XSLT(xsl_root)
     result_tree = transform(xml_tree)
-
-    # Convert the result to HTML
     html_content = etree.tostring(result_tree)
 
     with open("output.pdf", "wb") as pdf_output:
@@ -27,12 +18,23 @@ def convert(xml_file: str, xsl_file: str):
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--xml", required=True)
-    parser.add_argument("--xsl", required=True)
+    parser.add_argument("xml")
+    parser.add_argument("--xsl", default="sdi", choices=["sdi"])
 
     args = parser.parse_args(argv)
 
-    convert(args.xml, args.xsl)
+    xsl_file = os.path.join(os.path.dirname(__file__), f"styles/{args.xsl}.xsl")
+
+    try:
+        with open(args.xml, "rb") as xml_input:
+            xml_tree = etree.parse(xml_input)
+
+        with open(xsl_file, "rb") as xsl_input:
+            xsl_root = etree.XML(xsl_input.read())
+    except FileNotFoundError as exc:
+        raise SystemExit("Cannot open input files.") from exc
+
+    convert(xml_tree, xsl_root)
 
 
 if __name__ == "__main__":
---
+++ src/styles/__init__.py
@@ -0,0 +1,7 @@
+"""
+Styles file from several providers such as SDI, Aruba, ...
+
+
+# Sources
+- SDI: https://www.fatturapa.gov.it/export/documenti/fatturapa/v1.2.2/Foglio_di_stile_fattura_ordinaria_ver1.2.2.xsl
+"""
